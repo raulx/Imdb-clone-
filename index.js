13b21e2c60eecdbd0c8f0a4545461f6e80cf817e
Raulx103 <raulx103@gmail.com>
2023-03-26 22:16:47 +0530

    adding search option overlay

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const menuToggle = document.getElementById('menu-toggle');
 const overlayBtn = document.getElementById('overlay-toggle');
 const overlayContainer = document.querySelector('.overlay-container');
+const searchBar = document.getElementById('search-bar');
+const searchInput = document.getElementById('search-input');
+
+searchInput.addEventListener('focus',function(){
+    searchBar.classList.add('focus')
+})
+searchInput.addEventListener('blur',function(){
+    searchBar.classList.remove('focus')
+})
 function toggleOverlay(){
     overlayContainer.classList.toggle('overlay-active')
 }
